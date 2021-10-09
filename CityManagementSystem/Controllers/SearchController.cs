c5b7a40a97d0ae573a70dec805aeaf97f8c2d8dc
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2021-10-10 00:46:38 +0600

    Countries Searching & Paging DOne

--- CityManagementSystem/Controllers/SearchController.cs
+++ CityManagementSystem/Controllers/SearchController.cs
@@ -9,6 +9,7 @@ using PagedList.Mvc;
 using PagedList;
 using System.Data.Entity;
 using Microsoft.Ajax.Utilities;
+using System.Collections;
 
 namespace CityManagementSystem.Controllers
 {
@@ -16,7 +17,7 @@ namespace CityManagementSystem.Controllers
     {
         ManagementSystemContext context = new ManagementSystemContext();
         // GET: Search
-        public ActionResult Search(string cityName, int? countryId, int? pageNo, int pageSize = 3)
+        public ActionResult Search(string countryName, int? countryId, int? pageNo, int pageSize = 3)
         {
             pageNo = pageNo ?? 1;
             int skipCount = (int)(pageSize * (pageNo - 1));
@@ -25,15 +26,15 @@ namespace CityManagementSystem.Controllers
 
             CityDetailsViewModel viewModel = new CityDetailsViewModel();
 
-            viewModel.CityName = cityName;
+            viewModel.CountryName = countryName;
             viewModel.CountryId = countryId;
             viewModel.Countries = context.Countries.ToList();
 
             var citydetail = context.Cities.Include(c=> c.Country).AsQueryable();
 
-            if (!string.IsNullOrEmpty(cityName))
+            if (!string.IsNullOrEmpty(countryName))
             {
-                citydetail = citydetail.Where(c => c.Name.ToLower().Contains(cityName.ToLower()));
+                citydetail = citydetail.Where(c => c.Name.ToLower().Contains(countryName.ToLower()));
             }
 
             if (countryId.HasValue && countryId.Value > 0)
@@ -49,9 +50,36 @@ namespace CityManagementSystem.Controllers
             viewModel.Cities = citydetail.OrderByDescending(c => c.Name).Skip(skipCount).Take(pageSize).ToList();
 
             return View(viewModel);
-        } 
+        }
 
-        
+        public ActionResult CountrySearch(string countryName,int? pageNo, int pageSize = 3)
+        {
+            pageNo = pageNo ?? 1;
+            int skipCount = (int)(pageSize * (pageNo - 1));
+
+
+            CountryDetailsViewModel viewModel = new CountryDetailsViewModel();
+
+            viewModel.CountryName = countryName;
+            
+
+            var countrydetail = context.Countries.AsQueryable();
+
+            if (!string.IsNullOrEmpty(countryName))
+            {
+                countrydetail = countrydetail.Where(c => c.Name.ToLower().Contains(countryName.ToLower()));
+            }
+
+            
+            var totalcountry = countrydetail.Count();
+            
+            viewModel.Pager = new Pager(totalcountry, pageNo, pageSize);
+
+            viewModel.Countries = countrydetail.OrderBy(c => c.Name).Skip(skipCount).Take(pageSize).Include(c=>c.Cities).ToList();
+            
+            return View(viewModel);
+
+        }
 
     }
 }
---
+++ CityManagementSystem/ViewModel/CountryDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CityManagementSystem.Models;
+
+namespace CityManagementSystem.ViewModel
+{
+    public class CountryDetailsViewModel
+    {
+        public string CountryName { get; set; }
+        public List<Country> Countries { get; set; }
+        public List<City> Cities { get; set; }
+        public Pager Pager { get; set; }
+    }
+}
